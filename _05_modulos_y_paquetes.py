b950817ab24b42e79bcd1a3986d19acb5e831991
Rodrigo Alvarez <rodrigo.alvare.herrera@gmail.com>
2024-10-17 16:19:03 -0600

    (main): Modulos, paquetes y uso de __init__.py

---
+++ _05_modulos_y_paquetes.py
@@ -0,0 +1,21 @@
+"""MÓDULOS Y PAQUETES
+
+1. Módulos
+Permiten organizar nuestro código especialmente en proyectos de gran tamaño. Un módulo
+es un archivo de python con funciones, clases y objetos en su interior.
+
+Los objetos dentro de un módulo puede ser importado desde otros scripts en python.
+
+Usamos la palabra import nombre_modulo para importar un módulo, para acceder a los
+objetos en su interior usamos:
+
+import nombre_modulo
+
+nombre_modulo.member
+
+2. Paquetes
+Un paquete es un directorio que contiene múltiples módulos y sub paquetes. Un paquete
+debe contener un archivo __init__.py
+
+En este archivo podemos especificar qué es lo que se exportará al exterior  
+"""
